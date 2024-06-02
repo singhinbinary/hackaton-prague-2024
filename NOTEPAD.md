ba2a02d079d81d2be8fa676dcee285b81b95eae7
singhinbinary <singhinbinary@users.noreply.github.com>
2024-06-02 08:30:31 +0200

    chore: build sample website for vercel

---
+++ NOTEPAD.md
@@ -0,0 +1,3 @@
+# Notepad
+
+Data from graphql in JSON processed in CSV, then injected in D3.js + Neo 4J
---
+++ src/components/LeftColumn/LeftColumn.tsx
@@ -0,0 +1,20 @@
+import { ConnectKitButton } from 'connectkit';
+
+import Example from '../Example';
+
+export default function LeftColumn() {
+  return (
+    <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+      <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+        Instructions
+      </h1>
+      {/* <ConnectKitButton /> */}
+
+      <p className="leading-relaxed mb-3">
+        Click on the right button. This will load neo4j data and display a graph
+      </p>
+
+      <Example />
+    </div>
+  );
+}
